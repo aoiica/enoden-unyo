3e6a7252b8dea68ea223ce31af81de9b53483705
aoiica <kamacrush@gmail.com>
2020-10-10 17:40:06 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -3,6 +3,6 @@ var uny1='[1]1501F(セーリング)'
 var uny2='[2]1502F+1101F(情報発信)'
 var uny3='[3]22F(グッズポテト)+1002F(コカコーラ)【R】'
 var uny4='[4]1001F+10F'
-var uny5='[5]305F(HM)'
+var uny5='[5]2002F【R】（305F車両交換）</h2> <p'
 var uny6='[6]2003F'
 var twDay=10
