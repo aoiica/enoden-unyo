6dc6d87bb0c01fea38a41175b8d1f99b7b2c83f7
aoiica <kamacrush@gmail.com>
2020-11-15 20:40:07 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,1 +1,8 @@
 
+var uny1='[1]1101F+305F【R】'
+var uny2='[2]2001F+2003F【R】'
+var uny3='[3]1501F+1002F【N】'
+var uny4='[4]22F+10F【R】'
+var uny5='[5]501F+1001F【R】'
+var uny6='[6]2002F+1502F【UR】</p></div><div'
+var twDay=15
