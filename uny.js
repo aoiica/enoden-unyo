bae311440b6d2cf49ac506087a6f41ca05bcb31a
aoiica <kamacrush@gmail.com>
2020-11-20 19:40:07 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,5 +1,5 @@
 
-var uny1='[1]502F(HM)+21F[全パン]'
+var uny1='[1]21F&lt;K↑&gt;'
 var uny2='[2]2001F(HM)+2002F【R】'
 var uny3='[3]1501F(セーリング)+1002F(コカコーラ)【N】'
 var uny4='[4]501F(きのたけ)+10F&lt;全&gt;【PR】'
