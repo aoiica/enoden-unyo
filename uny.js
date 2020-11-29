bacce3577b173d593ba12fdf1fa06e06f8316cfc
aoiica <kamacrush@gmail.com>
2020-11-29 16:10:06 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
-var uny1='[1]1101F(情報発信)+2001F(HM)【R】'
-var uny2='[2]10F+1502F【N】'
-var uny3='[3]501F(きのたけ)+2003F&lt;全&gt;【R】'
+var uny1='[1]1101F(情報発信)+2001F(HM)'
+var uny2='[2]10F+1502F'
+var uny3='[3]501F(きのたけ)+2003F[全パン]'
 var uny4='[4]305F(HM)'
-var uny5='[5]502F(HM)+21F&lt;全&gt;【R】'
-var uny6='[6]22F(グッズポテト)+1001F(カレンダー)【R】'
+var uny5='[5]502F(HM)+21F[全パン]'
+var uny6='[6]22F(グッズポテト)+1001F(カレンダー)'
 var twDay=29
