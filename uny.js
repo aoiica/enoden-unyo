7922abf07093ad4d55e9e02e31f9270cbbd14413
aoiica <kamacrush@gmail.com>
2021-02-12 10:40:07 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
-var uny1='[1]10F</p></div><div'
+var uny1='[1]21F+501F(きのたけ)【R】'
 var uny2='[2]305F+1001F【N】'
 var uny3='[3]1101F+1002F(コカコーラ)【R】'
 var uny4='[4]1501F(セーリング・文字幕)</p></div><div'
 var uny5='[5]1501F(セーリング)+502F'
-var uny6='[6]2001F+2003F'
+var uny6='[6]2003F+1501F(セーリング)【R】'
 var twDay=12
