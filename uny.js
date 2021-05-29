d6c58ae3d7fd2bb73417a8be5bf27d0e748eeca4
aoiica <kamacrush@gmail.com>
2021-05-30 07:40:07 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -4,5 +4,5 @@ var uny2='[2]1001F'
 var uny3='[3]21F+502F'
 var uny4='[4]1201F'
 var uny5='[5]1002F(コカコーラ)+1502F'
-var uny6='[6]22F+501F→1101F【SR】車両交換</p></div><div'
-var twDay=29
+var uny6='[6]10F+2001F'
+var twDay=30
