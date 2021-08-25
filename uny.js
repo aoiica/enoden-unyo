7944f572e3823b05da46d4b67334cd8420f756c9
aoiica <kamacrush@gmail.com>
2021-08-26 07:10:06 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
-var uny1='[1]305F+1002F(コカコーラ)'
-var uny2='[2]1501F+2002F'
-var uny3='[3]1502F+1101F'
-var uny4='[4]1001F+501F(きのたけ)'
-var uny5='[5]10F(10排障器無)+22F(グッズポテト)'
-var uny6='[6]502F+21F【R】</p></div><div'
-var twDay=25
+var uny1='[1]22F(グッズポテト)+10F(10排障器無)'
+var uny2='[2]305F+1101F'
+var uny3='[3]1001F+501F(きのたけ)'
+var uny4='[4]1502F+502F'
+var uny5='[5]2001F(みずたま)+2003F'
+var uny6='[6]1501F+2002F'
+var twDay=26
