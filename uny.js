28d279d847c7c4356bbd9f1975cdd8234bc5f509
aoiica <kamacrush@gmail.com>
2021-11-17 19:10:05 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -4,5 +4,5 @@ var uny2='[2]1501F+1002F(コカコーラ)'
 var uny3='[3]1001F+1101F(観音電車)'
 var uny4='[4]2002F+501F(きのたけ)'
 var uny5='[5]10F(10排障器無)+22F(グッズポテト)'
-var uny6='[6]502F+21F【R】</p></div><div'
+var uny6='[6]502F+21F'
 var twDay=17
