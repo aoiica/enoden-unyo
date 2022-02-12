8a02f79dc0ccf87255dee9933a2fcc0e2bf194e1
aoiica <kamacrush@gmail.com>
2022-02-12 23:10:08 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
-var uny1='[1]10F+1502F【N】'
+var uny1='[1]10F(検車区#2'
 var uny2='[2]1201F+1501F【R】'
 var uny3='[3]502F(きのたけ)'
 var uny4='[4]2003F+2001F【R】'
-var uny5='[5]1101F(観音電車)'
-var uny6='[6]1002F(コカコーラ)'
+var uny5='[5]1101F(観音電車)&lt;K↑&gt;'
+var uny6='[6]1002F(コカコーラ)&lt;全&gt;'
 var twDay=12
