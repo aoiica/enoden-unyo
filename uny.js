ccce5d1b83364166d90840b00759f9c0646f8aba
aoiica <kamacrush@gmail.com>
2022-09-07 10:40:07 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -4,5 +4,5 @@ var uny2='[2]1002F(コカコーラ)+2002F'
 var uny3='[3]2002F&lt;全&gt;</p></div><div'
 var uny4='[4]1501F+22F(グッズポテト)'
 var uny5='[5]1201F+305F(コスモス)'
-var uny6='[6]502F(きのたけ)+21F【N】'
+var uny6='[6]502F(きのたけ)+21F【N】</p></div><div'
 var twDay=6
