d799b255868ba7466f02ad980402c2a4da4075df
aoiica <kamacrush@gmail.com>
2022-09-20 06:10:07 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
 var uny1='[1]1101F(SEA'
-var uny2='[2]1002F(コカコーラ)+2002F'
-var uny3='[3]2001F(大河)'
-var uny4='[4]2003F(ギャラリー)+501F'
+var uny2='[2]1502F'
+var uny3='[3]2003F(ギャラリー)+501F'
+var uny4='[4]1001F'
 var uny5='[5]502F(きのたけ)'
-var uny6='[6]1001F'
-var twDay=19
+var uny6='[6]1002F(コカコーラ)+2002F</p></div><div'
+var twDay=20
