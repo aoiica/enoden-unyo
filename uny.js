e5c099a3d45633d72a9753d03c5ede5730b0a855
aoiica <kamacrush@gmail.com>
2022-10-04 20:10:09 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
-var uny1='[1]10F+21F【R】'
-var uny2='[2]305F(ハロウィン)+1001F【N】'
-var uny3='[3]501F+502F(きのたけ)'
+var uny1='[1]21F'
+var uny2='[2]305F(ハロウィン)+1001F'
+var uny3='[3]22F(グッズポテト)&lt;全&gt;'
 var uny4='[4]2002F+1201F'
-var uny5='[5]1502F【SSR】'
+var uny5='[5]502F(きのたけ)&lt;全&gt;'
 var uny6='[6]1501F+2001F(大河)【SR】'
 var twDay=4
