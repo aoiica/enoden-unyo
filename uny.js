ff64120011ca77e31e2a35936747c667392dd41c
aoiica <kamacrush@gmail.com>
2022-11-29 04:10:05 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,7 +1,7 @@
 
-var uny1='[1]502F(きのたけ)</p></div><div'
+var uny1='[1]502F(きのたけ)'
 var uny2='[2]21F+10F'
-var uny3='[3]1201F+2001F(大河)'
+var uny3='[3]2001F(大河)'
 var uny4='[4]1101F+1002F(コカコーラ)'
 var uny5='[5]501F+2003F(ギャラリー)'
 var uny6='[6]1502F</p></div><div'
