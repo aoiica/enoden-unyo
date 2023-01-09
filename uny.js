a32435f916659412f76d8079dc86a19b5528c9a4
aoiica <kamacrush@gmail.com>
2023-01-09 23:40:06 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -2,7 +2,7 @@
 var uny1='[1]1502F'
 var uny2='[2]10F+502F(きのたけ)'
 var uny3='[3]2002F'
-var uny4='[4]21Fの単行発生の流れは情報をまとめると以下の通りだと思われる。'
+var uny4='[4]1101F'
 var uny5='[5]1201F'
 var uny6='[6]1501F+21F</p></div><div'
 var twDay=9
