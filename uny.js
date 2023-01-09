5d980d4a76fcd6f98edbbbd75d0d8be6ec6b6057
aoiica <kamacrush@gmail.com>
2023-01-09 22:40:08 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -1,8 +1,8 @@
 
-var uny1='[1]2001F+501F【R】'
-var uny2='[2]10F+502F(きのたけ)【R】'
-var uny3='[3]1201F+2003F(ギャラリー)【R】'
+var uny1='[1]1502F'
+var uny2='[2]10F+502F(きのたけ)'
+var uny3='[3]2002F'
 var uny4='[4]21Fの単行発生の流れは情報をまとめると以下の通りだと思われる。'
-var uny5='[5]1101F+2002F【R】'
-var uny6='[6]1502F+1002F(コカコーラ)【R】'
+var uny5='[5]1201F'
+var uny6='[6]1501F+21F</p></div><div'
 var twDay=9
