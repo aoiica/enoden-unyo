5e9e1379d007fb0a4ca19cf9e6735a1862234454
aoiica <kamacrush@gmail.com>
2023-03-10 10:10:06 +0900

    cron: cron

--- uny.js
+++ uny.js
@@ -4,5 +4,5 @@ var uny2='[2]305F+1001F【N】'
 var uny3='[3]502F(きのたけ)+21F【N】'
 var uny4='[4]2002F+1002F(コカコーラ)【R】'
 var uny5='[5]1502F+1201F【R】'
-var uny6='[6]305F+1001F'
+var uny6='[6]1101F+501F</p></div><div'
 var twDay=9
